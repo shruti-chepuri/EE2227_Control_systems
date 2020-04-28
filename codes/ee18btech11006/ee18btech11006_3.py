cd3d12bb7ed8d995a64f4085c840eaec4df9819f
shruti-chepuri <61222184+shruti-chepuri@users.noreply.github.com>
2020-04-29 00:27:33 +0530

    Update ee18btech11006_3.py

--- codes/ee18btech11006/ee18btech11006_3.py
+++ codes/ee18btech11006/ee18btech11006_3.py
@@ -1,18 +1,27 @@
+'''
+Code by C Shruti
+April 27,2020
+Released under GNU GPL
+'''
 import numpy as np
 import math as mp
 import matplotlib.pyplot as plt
 from scipy import signal
 import control
 from control import tf
+#if using termux
 import subprocess
 import shlex
+#end if
 system = signal.lti([1*0.1778,8.5*0.1778,15*0.1778], [1,0.8,0.07])
 r = np.linspace(0.1, 100,1001)
 w, mag, phase = signal.bode(system, w=r)
 plt.semilogx(w,(mag),'r')    # Bode magnitude plot
 plt.ylabel("[dB]")
 plt.xlabel("[rad/s]")
+#if using termux
 plt.savefig('./figs/ee18btech11006/ee18btech11006_3.pdf')
 plt.savefig('./figs/ee18btech11006/ee18btech11006_3.eps')
 subprocess.run(shlex.split("termux-open ./figs/ee18btech11006/ee18btech11006_3.pdf"))
+#end if
 #plt.show()
